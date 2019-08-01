85fd683990cb2b0b54d60e2dcfce48b76a1c9f9b
Daniel Richter <doener123@googlemail.com>
2019-08-01 15:36:44 +0200

    added nav bar

--- src/angular/app/app.component.ts
+++ src/angular/app/app.component.ts
@@ -1,17 +1,47 @@
-import { Component } from "@angular/core";
-import { ElectronService } from "./services/electron.service";
+import {
+  Component,
+  ChangeDetectorRef,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+  AfterViewInit
+} from "@angular/core";
+import { VERSION } from "@angular/material";
+import { NavService } from "./services/nav.service";
+import { NavItem } from "./nav-item";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  public isActive = false;
-  constructor(private electronService: ElectronService) {
-    return;
-  }
-  ToogleSidebar(){
-    this.isActive = !this.isActive;
+export class AppComponent implements AfterViewInit {
+  @ViewChild("appDrawer") appDrawer: ElementRef;
+  version = VERSION;
+  navItems: NavItem[] = [
+    {
+      displayName: "Home",
+      iconName: "recent_actors",
+      route: "devfestfl",
+      children: []
+    },
+    {
+      displayName: "Customizing",
+      iconName: "recent_actors",
+      route: "devfestfl",
+      children: [
+        {
+          displayName: "FileSystem",
+          iconName: "recent_actors",
+          route: "customizing/file-system"
+        }
+      ]
+    }
+  ];
+  constructor(private navService: NavService) {}
+
+  ngAfterViewInit() {
+    this.navService.appDrawer = this.appDrawer;
   }
 }
--- src/angular/app/app.module.ts
+++ src/angular/app/app.module.ts
@@ -1,26 +1,126 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import {ServerModule} from "@angular/platform-server";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
-import { HttpClientModule } from "@angular/common/http";
-
-import { AppRoutingModule } from "./modules/app-routing.module";
-
-import { ElectronService } from "./services/electron.service";
-
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FTPService } from "./services/ftp.service";
-import { MatTableModule } from "@angular/material/table";
-import { TypeORMService } from "./services/TypeOrm.service";
+import {A11yModule} from "@angular/cdk/a11y";
+import {BidiModule} from "@angular/cdk/bidi";
+import {ObserversModule} from "@angular/cdk/observers";
+import {OverlayModule} from "@angular/cdk/overlay";
+import {PlatformModule} from "@angular/cdk/platform";
+import {PortalModule} from "@angular/cdk/portal";
+import {ScrollDispatchModule} from "@angular/cdk/scrolling";
+import {CdkStepperModule} from "@angular/cdk/stepper";
+import {CdkTableModule} from "@angular/cdk/table";
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTableModule
+} from "@angular/material";
+import {AppComponent} from "./app.component";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MenuListItemComponent} from "./menu-list-item/menu-list-item.component";
+import {AppRoutingModule} from "./modules/app-routing.module";
+import { NavService } from "./services/nav.service";
+import { TopNavComponent } from "./top-nav/top-nav.component";
 import { FileSystemComponent } from "./file-system/file-system.component";
+import { HomeComponent } from "./home/home.component";
+
+/**
+ * NgModule that includes all Material modules that are required.
+ */
+@NgModule({
+  exports: [
+    // CDK
+    A11yModule,
+    BidiModule,
+    ObserversModule,
+    OverlayModule,
+    PlatformModule,
+    PortalModule,
+    ScrollDispatchModule,
+    CdkStepperModule,
+    CdkTableModule,
+
+    // Material
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+  ]
+})
+export class MaterialModule {}
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FileSystemComponent],
-  imports: [AppRoutingModule, HttpClientModule, MatTableModule,
-    ServerModule, BrowserModule.withServerTransition({appId: "decentral-mediaserver"}), BrowserAnimationsModule],
-  providers: [ElectronService, FTPService, TypeORMService],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    MenuListItemComponent,
+    HomeComponent,
+    FileSystemComponent,
+    TopNavComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [NavService]
 })
 export class AppModule {}
